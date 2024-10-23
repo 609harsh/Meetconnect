c75f6879ceee4897415dec5a0a996bd35dc69bd7
609harsh <harshgupta609@gmail.com>
2024-10-24 00:11:53 +0530

    fix: validation errors fix

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,6 +1,68 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+  const [error, setError] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+
+  useEffect(() => {
+    setError("");
+  }, [email, password]);
+  const submitForm = () => {
+    if (!email || !password || !name || !phoneNumber) {
+      setError("All fields required");
+      return;
+    }
+    if (name.length <= 2) {
+      setError("Name too short");
+      return;
+    }
+    if (name.length >= 30) {
+      setName("Name too long");
+      return;
+    }
+    if (name.match(/(?=.*\d)/)) {
+      setError("Name cannot have digits");
+      return;
+    }
+    if (
+      !email
+        .trim()
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+      setError("Please enter valid email");
+      return;
+    }
+    if (password.length < 8) {
+      setError("Password length should be at least 8 charcters");
+      return;
+    }
+    if (!password.match(/(?=.*[a-z])/)) {
+      setError("Password should have at least one lowercase letter");
+      return;
+    }
+    if (!password.match(/(?=.*[A-Z])/)) {
+      setError("Password should have at least one uppercase letter");
+      return;
+    }
+    if (!password.match(/(?=.*\d)/)) {
+      setError("Password should have at least one digit");
+      return;
+    }
+    if (!password.match(/(?=.*[@$!%*?&])/)) {
+      setError("Password should have at least one special character");
+      return;
+    }
+    //Write api call
+    console.log(email, password);
+  };
+
   return (
     <div className="w-full h-full flex justify-center items-center">
       <div className="flex w-full max-w-sm overflow-hidden bg-white rounded-lg shadow-lg  lg:max-w-5xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -75,6 +137,7 @@ const Signin = () => {
             <input
               id="FullName"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg   focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+              onChange={(e) => setName(e.target.value)}
             />
           </div>
           <div className="mt-4">
@@ -88,6 +151,7 @@ const Signin = () => {
               id="LoggingEmailAddress"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg   focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
               type="email"
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="mt-4">
@@ -100,6 +164,7 @@ const Signin = () => {
             <input
               id="PhoneNumber"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg   focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+              onChange={(e) => setPhoneNumber(e.target.value)}
             />
           </div>
           <div className="mt-4">
@@ -116,11 +181,17 @@ const Signin = () => {
               id="loggingPassword"
               className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg  focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
               type="password"
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
 
+          {error !== "" && <p className="text-sm text-red-600 mt-1">{error}</p>}
+
           <div className="mt-6">
-            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+            <button
+              onClick={() => submitForm()}
+              className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+            >
               Sign Up
             </button>
           </div>
