b83988e432db24ccf10823134e469e0e775e7047
609harsh <harshgupta609@gmail.com>
2024-12-04 18:21:11 +0530

    created app.ts and index.ts files

--- server-api/src/index.ts
+++ server-api/src/index.ts
@@ -1,14 +1,12 @@
-import express, { Express, Request, Response } from "express";
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
+import app from "./app";
+import express from "express";
+dotenv.config();
 
-// dotenv.config();
-
-const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("eScript Server");
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
