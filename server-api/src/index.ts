4be8026e7e1f3717839ea5ed887b4b3c8089e28e
609harsh <harshgupta609@gmail.com>
2024-12-04 22:45:39 +0530

    resources endpoint created

--- server-api/src/index.ts
+++ server-api/src/index.ts
@@ -5,9 +5,6 @@ dotenv.config();
 
 const port = process.env.PORT || 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
