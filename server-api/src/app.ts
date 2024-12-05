4a2ad38b3844ac82c29732a46b31fe76abb2a0b9
609harsh <harshgupta609@gmail.com>
2024-12-05 11:51:32 +0530

    fix: cors error

--- server-api/src/app.ts
+++ server-api/src/app.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response } from "express";
 import router from "./routes/routes";
+import cors from "cors";
 const app: Express = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 export default app;
