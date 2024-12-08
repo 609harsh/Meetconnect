75b5700d47ec6924342908ec22f8b85e97aad474
609harsh <harshgupta609@gmail.com>
2024-12-08 18:57:07 +0530

    env update for api_url

--- client/src/redux/meetApi.ts
+++ client/src/redux/meetApi.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { Interview, Resources } from "../types";
+const api_url: string = import.meta.env.VITE_API_HOST;
 
 export interface ApiResponse<T> {
   success: boolean;
@@ -14,7 +15,7 @@ export interface ApiUserResponse {
 export const meetApi = createApi({
   reducerPath: "meetApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/",
+    baseUrl: api_url,
   }),
   endpoints: (builder) => ({
     getInterviews: builder.query<Interview[], void>({
