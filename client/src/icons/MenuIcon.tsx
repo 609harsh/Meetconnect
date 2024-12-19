cd44821360083f4eded4a78a134efc9fa3c16e95
609harsh <harshgupta609@gmail.com>
2024-12-20 01:31:30 +0530

    Icons update

--- client/src/icons/MenuIcon.tsx
+++ client/src/icons/MenuIcon.tsx
@@ -1,20 +1,19 @@
-function CloseIcon() {
+function MenuIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
+      className="w-6 h-6"
       fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
       stroke="currentColor"
-      className="size-6"
+      stroke-width="2"
     >
       <path
         strokeLinecap="round"
         strokeLinejoin="round"
-        d="M6 18 18 6M6 6l12 12"
-      />
+        d="M4 6h16M4 12h16M4 18h16"
+      ></path>
     </svg>
   );
 }
 
-export default CloseIcon;
+export default MenuIcon;
