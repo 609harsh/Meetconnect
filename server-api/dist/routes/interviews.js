b3e021ede61f7d4f8f1741090ae3ef0f916ac949
609harsh <harshgupta609@gmail.com>
2024-12-21 23:49:25 +0530

    removing vercel

--- server-api/dist/routes/interviews.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const interviews_1 = require("../controllers/interviews");
-const authorizationMiddleware_1 = require("../authorizationMiddleware");
-const interviews = (0, express_1.Router)();
-interviews.get("/interviews", authorizationMiddleware_1.authorization, interviews_1.interviewController.fetchInterviews);
-interviews.delete("/interviews/:id", authorizationMiddleware_1.authorization, interviews_1.interviewController.deleteInterviews);
-exports.default = interviews;
