5b9c136e93b1f0fc3ba043eb206672206bb57d83
ABDUL RAHIM <abdulrahim74264@zohomail.in>
2025-01-02 15:45:10 +0530

    Fix: Issue 155 Fixed

--- client/src/components/Tracker/AddJob.tsx
+++ client/src/components/Tracker/AddJob.tsx
@@ -205,6 +205,7 @@ const AddJob = ({
               </button>
               <button
                 type="submit"
+                disabled={isButtonDisabled}
                 className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Save
